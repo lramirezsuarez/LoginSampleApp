c48e37f009439c894fc4f5f7785b875b11f5588c
Luis Ramirez <lramirezsuarez@gmail.com>
2025-01-20 18:45:05 -0500

    Initial Commit

---
+++ LoginSample/LoginSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginSampleApp.swift
+//  LoginSample
+//
+//  Created by Luis Alejandro Ramirez Suarez on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct LoginSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
